a1a608bd400af6b8025d68d4f7cdb7bc05c207b2
alexcompton <alexcompton11@gmail.com>
2016-02-23 20:15:17 -0500

    cloud server update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://localhost:27017/myDatabase',
+	'database': 'mongodb://alexcompton11:A1s2d3f4!@ds015478.mongolab.com:15478/compton-node',
 	'secret': 'ilovescotchscotchyscotchscotch'
 };
